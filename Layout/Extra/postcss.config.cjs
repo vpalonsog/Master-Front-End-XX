4042db9a06dcd68be2fb1a603e41361ac68cc807
Vanessa Alonso G <vpalonsog@gmail.com>
2026-06-02 17:12:37 -0500

    Fix(package-lock.json, package.json, postcss.config.cjs, main.css, media.css, variables.css, aside.css, container.css): Add queries and fix styles and packages

--- Layout/Extra/postcss.config.cjs
+++ Layout/Extra/postcss.config.cjs
@@ -4,13 +4,18 @@ const postcssNested = require('postcss-nested');
 const postcssImport = require('postcss-import');
 const simpleVars = require('postcss-simple-vars');
 const postcssMixins = require('postcss-mixins');
+const postcssCustomMedia = require('postcss-custom-media');
 
 
 module.exports = {
   plugins:[
     postcssImport(),
+    postcssCustomMedia({ preserve: false }),
     postcssPresetEnv({
-        stage: 2
+      stage: 2,
+      features: {
+        'custom-media-queries': false,
+      }
     }),
     postcssNested(),
     simpleVars(),
