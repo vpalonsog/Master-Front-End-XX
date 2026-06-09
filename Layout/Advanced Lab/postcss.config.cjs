232f05d4ce6065f103088738fc30c14d6cab3fbc
Vanessa Alonso G <vpalonsog@gmail.com>
2026-06-09 15:37:21 -0500

    Add(structure files): Add 'Advanced Lab' folder

---
+++ Layout/Advanced Lab/postcss.config.cjs
@@ -0,0 +1,32 @@
+const postcssPresetEnv = require('postcss-preset-env');
+const cssnano = require('cssnano');
+const postcssNested = require('postcss-nested');
+const postcssImport = require('postcss-import');
+const simpleVars = require('postcss-simple-vars');
+const postcssMixins = require('postcss-mixins');
+const postcssCustomMedia = require('postcss-custom-media');
+
+
+module.exports = {
+  plugins:[
+    postcssImport(),
+    postcssCustomMedia({ preserve: false }),
+    postcssPresetEnv({
+      stage: 2,
+      features: {
+        'custom-media-queries': false,
+      }
+    }),
+    postcssNested(),
+    simpleVars(),
+    postcssMixins(),
+    cssnano({
+        preset: [
+            'default', {
+                calc:false,
+            }
+        ]
+    })
+  ] 
+  
+}
