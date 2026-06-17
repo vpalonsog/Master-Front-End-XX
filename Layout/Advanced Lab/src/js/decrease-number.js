5023d1602192e994279af31d10a4bee5dfe1c65c
Vanessa Alonso G <vpalonsog@gmail.com>
2026-06-17 17:04:30 -0500

    Feat(index.html, main.css, media.css, container-css, footer.css, decrease-number.js): Add Footer layout, update styles and js file according to media queries

--- Layout/Advanced Lab/src/js/decrease-number.js
+++ Layout/Advanced Lab/src/js/decrease-number.js
@@ -1,11 +1,11 @@
 const span = document.getElementById('top__text');
 
-if(window.innerWidth < 1280){
+if(window.innerWidth < 1024){
     span.textContent = '3';
 }
 
 window.addEventListener('resize', () => {
-    if(window.innerWidth >= 1280){
+    if(window.innerWidth >= 1024){
         span.textContent = '5';
     }
 });
