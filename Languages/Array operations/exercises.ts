b60232ebe35b56bc7c525f9ba6a580751ac25e7c
Vanessa Alonso G <vpalonsog@gmail.com>
2026-07-27 12:03:14 -0500

    Feat(exercises.ts): Add tail and init function

--- Languages/Array operations/exercises.ts
+++ Languages/Array operations/exercises.ts
@@ -8,3 +8,26 @@ const head = <T>(array: Array<T>): T =>{
 head(['Hola', 'mi', 'nombre', 'es', 'Vanessa']) /* R/ 'Hola' */
 
 
+/*Tail*/
+const tail = <T>(array: Array<T>): Array<T> => {
+    const [, ...restOfArray] = array;
+    return restOfArray;
+}
+
+tail(['Hola', 'mi', 'nombre', 'es', 'Vanessa']) /* R/ ['mi', 'nombre', 'es', 'Vanessa'] */
+
+
+/*Init*/
+
+const init = <T>(array: Array<T>): Array<T> => {
+    const arraySliced = array.slice(0, -1);
+    return arraySliced; 
+
+}
+
+init(['Inicio', 'mitad', 'final']); /* R/ ['Inicio', 'mitad'] */
+
+
+
+
+
